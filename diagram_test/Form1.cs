e040389d90e5a3cbd841057bcf4157ac35177dda
Florence Fitchett <244646@student.wqe.ac.uk>
2025-12-05 16:09:20 +0000

    added first particle

--- diagram_test/Form1.cs
+++ diagram_test/Form1.cs
@@ -2,9 +2,26 @@ namespace diagram_test
 {
     public partial class Form1 : Form
     {
+        public Particle p;
+        
+
+
         public Form1()
         {
             InitializeComponent();
+            p = new Particle(20, 20, 20, 20, Properties.Resources.circle);
+        }
+        
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(p.getImage(), p.getX(), p.getY(), p.getWidth(), p.getHeight());
         }
     }
 }
---
+++ diagram_test/Particle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace diagram_test
+{
+    public class Particle
+    {
+        //attributes
+        protected int x;
+        protected int y;
+        protected int width;
+        protected int height;
+        protected Image image;
+        
+        //constructor
+        public Particle(int x, int y, int width, int height, Image image)
+        {
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+            this.image = image;
+        }
+
+        //getters and setters
+        public int getX(){ return x;}
+        public int getY(){ return y;}
+        public int getWidth() { return width;}
+        public int getHeight() { return height;}
+        public Image getImage() {return image;}
+
+
+    }
+}
