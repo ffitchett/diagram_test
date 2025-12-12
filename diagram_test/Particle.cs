566714429a37378ef16054c6a759acc009fb5bdf
Florence Fitchett <244646@student.wqe.ac.uk>
2025-12-12 16:08:00 +0000

    added speed

--- diagram_test/Particle.cs
+++ diagram_test/Particle.cs
@@ -13,10 +13,12 @@ namespace diagram_test
         protected int y;
         protected int width;
         protected int height;
+        protected int xspeed;
+        protected int yspeed;
         protected Image image;
         
         //constructor
-        public Particle(int x, int y, int width, int height, Image image)
+        public Particle(int x, int y, int width, int height, int xspeed, int yspeed, Image image)
         {
             this.x = x;
             this.y = y;
@@ -30,7 +32,31 @@ namespace diagram_test
         public int getY(){ return y;}
         public int getWidth() { return width;}
         public int getHeight() { return height;}
+        public int getXspeed() { return xspeed;}
+
+        public int getYspeed() { return yspeed;}
         public Image getImage() {return image;}
+
+        // calculates the rate at which particles should move horizontally to reach a target in an amount of time
+        public int FindXspeed(int targetX, int particleX, int time_ratio)
+        {
+            int Xdistance = targetX - particleX;
+            if (Xdistance <= 0) { Xdistance = -Xdistance; }
+            return (Xdistance / time_ratio);
+        }
+
+        // calculates the rate at which particles should move vertically to reach a target in an amount of time
+        public int FindYspeed(int targetY, int particleY, int time_ratio)
+        {
+            int Ydistance = targetY - particleY;
+            if (Ydistance <= 0) { Ydistance = -Ydistance; }
+            return (Ydistance / time_ratio);
+        }
+
+        public void Update()
+        {
+
+        }
 
 
     }
